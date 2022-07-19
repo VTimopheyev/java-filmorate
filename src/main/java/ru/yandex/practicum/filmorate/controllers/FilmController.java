c81f69dc7f4a127308b1dbea06d418c17b8adb41
VTimopheyev <vtimopheyev@yandex.ru>
2022-07-19 16:08:10 +0300

    Created maven project

---
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.filmorate.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/films")
+public class FilmController {
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -0,0 +1,9 @@
+package ru.yandex.practicum.filmorate.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController ("/users")
+public class UserController {
+        
+
+}
